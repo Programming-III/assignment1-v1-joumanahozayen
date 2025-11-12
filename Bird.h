699626a3ee83bbd06e2c5bb6639ba84aa651de0e
joumanahozayen <joumana.hozayen@student.giu-uni.de>
2025-11-12 15:23:32 +0200

    completed course class

--- Bird.h
+++ Bird.h
@@ -1,14 +1,15 @@
 #ifndef BIRD_H
 #define BIRD_H
-
-#include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
-
+#include "animal.h"
+#include <iostream>
+using namespace std;
+class Bird:public Animal{
+    private:
+    float wingSpan;
+    
+    public:
+    Bird();
+    Bird(string name, int age,bool isHungry,float wingSpan);
+    ~Bird();
+};
 #endif
