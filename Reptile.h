5ca0beabaaedeb9b76d5ab2f192e07d5545b0d08
joumanahozayen <joumana.hozayen@student.giu-uni.de>
2025-11-12 15:26:01 +0200

    completed course class

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,15 @@
 #ifndef REPTILE_H
 #define REPTILE_H
-
-#include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
-
-
-
+#include "animal.h"
+#include <iostream>
+using namespace std;
+class Reptile:public Animal{
+    private:
+    bool isVenomous;
+    
+    public:
+    Reptile();
+    Reptile(string name, int age,bool isHungry,bool isVenomous);
+    ~Reptile();
+};
 #endif
