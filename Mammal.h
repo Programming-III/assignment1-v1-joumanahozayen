54cfeac44a8979b71fc92e15f1083c27f89dc4bb
joumanahozayen <joumana.hozayen@student.giu-uni.de>
2025-11-12 15:25:25 +0200

    completed course class

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,15 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
-#include "Animal.h"
-
-//define mammal class here
-
-
-
-
-
-
+#include "animal.h"
+#include <iostream>
+using namespace std;
+class Mammal :public Animal{
+    private:
+    string furColor;
+    
+    public:
+    Mammal();
+    Mammal(string name, int age,bool isHungry,string furColor);
+    ~Mammal();
+};
 #endif
